f08e2b08146a80566ba5b52c32e6da290ec14479
Simon Mika <simon@mika.se>
2016-04-01 11:07:57 +0200

    Switched to use generic ContentBlock as base class.

--- source/Typeup/UnorderedList.ts
+++ source/Typeup/UnorderedList.ts
@@ -4,18 +4,15 @@
 /// <reference path="ListItem" />
 
 module Cogneco.Typeup {
-	export class UnorderedList extends Block {
-		constructor(private items: ListItem[]) {
-			super(items.map(item => item.getRegion()).reduce((left, right) => left.merge(right)))
+	export class UnorderedList extends ContentBlock<ListItem> {
+		constructor(private content: ListItem[]) {
+			super(content)
 		}
 		render(renderer: Renderer): string {
-			return renderer.render("unordered list", { "content": this.items.map(item => item.render(renderer)).join("") })
-		}
-		toObject(): any {
-			return { "items": this.items.map(item => item.toObject()) }
+			return renderer.render("unordered list", { "content": this.content.map(item => item.render(renderer)).join("") })
 		}
 		toString() {
-			return this.items.map(item => item.toString(" - ")).join("\n")
+			return this.content.map(item => item.toString(" - ")).join("\n")
 		}
 		static parse(source: Source): Block[] {
 			var peeked = ""
@@ -34,7 +31,7 @@ module Cogneco.Typeup {
 				if (next && next.length > 0 && next[index] instanceof UnorderedList) {
 					while (index-- > 0)
 						next.shift()
-					next[0] = new UnorderedList([current].concat((<UnorderedList>next[0]).items))
+					next[0] = new UnorderedList([current].concat((<UnorderedList>next[0]).content))
 					result = next
 				} else {
 					result = [new UnorderedList([current])]
