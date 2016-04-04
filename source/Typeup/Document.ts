69e72dbd0c322d31ee4f157179dcaf6f4bae65c5
Simon Mika <simon@mika.se>
2016-04-04 14:21:31 +0200

    Added support for numbered lists.

--- source/Typeup/Document.ts
+++ source/Typeup/Document.ts
@@ -14,6 +14,7 @@ import "./Heading"
 import "./EmptyLine"
 import "./Assignment"
 import "./UnorderedList"
+import "./OrderedList"
 
 export class Document extends Node {
 	constructor(private content: Block[], region: Error.Region) {
---
+++ source/Typeup/OrderedList.ts
@@ -0,0 +1,48 @@
+import { Block } from "./Block"
+import { ContentBlock } from "./ContentBlock"
+import { Inline } from "./Inline"
+import { Source } from "./Source"
+import { Renderer } from "./Renderer"
+import { ListItem } from "./ListItem"
+import { EmptyLine } from "./EmptyLine"
+import { Paragraph } from "./Paragraph"
+
+export class OrderedList extends ContentBlock<ListItem> {
+	constructor(content: ListItem[]) {
+		super(content, content.map(content => content.getRegion()).reduce((left, right) => left.merge(right)))
+	}
+	render(renderer: Renderer): string {
+		return renderer.render("ordered list", { "content": this.getContent().map(item => item.render(renderer)).join("") })
+	}
+	toString() {
+		return this.getContent().map(item => item.toString("1. ")).join("\n")
+	}
+	static parse(source: Source): Block[] {
+		var peeked = ""
+		var p: string
+		while (p = source.peekIs(peeked + "\t"))
+			peeked = p
+		var result: Block[]
+		if (source.readIf(peeked + "1.")) {
+			while (source.peek().match(/\s/))
+				source.read()
+			var current = new ListItem(Block.parseAll(source.requirePrefix("\t")), source.mark())
+			var next = Block.parse(source)
+			var index = 0
+			while (next && next.length > 0 && next[index] instanceof EmptyLine)
+				index++
+			if (next && next.length > 0 && next[index] instanceof OrderedList) {
+				while (index-- > 0)
+					next.shift()
+				next[0] = new OrderedList([current].concat((<OrderedList>next[0]).getContent()))
+				result = next
+			} else {
+				result = [new OrderedList([current])]
+				if (next && next.length > 0)
+					result = result.concat(next)
+			}
+		}
+		return result
+	}
+}
+Block.addParser(OrderedList.parse)
