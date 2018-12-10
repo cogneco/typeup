abaf4006954f114c72c935d9cded86ef202fc6be
Simon Mika <simon@mika.se>
2018-12-10 19:37:14 +0100

    Updated built in template.

--- source/Typeup/Renderer.ts
+++ source/Typeup/Renderer.ts
@@ -5,25 +5,33 @@ export class Renderer {
 	private template: Template
 	constructor() {
 		const template: any = {
-			name: "document",
-			description: "Document template used for testing.",
-			output: "html",
-			tags: { /* tslint:disable:no-invalid-template-strings max-line-length */
+			"name": "document",
+			"description": "Document template used for testing.",
+			"output": "html",
+			"dependencies": [
+			],
+			"tags": { /* tslint:disable:no-invalid-template-strings max-line-length */
 				"*": "<h1><code>${content}</code></h1>",
-				document: "<!doctype html>\n<html>\n<head>\n<meta charset='UTF-8'>\n<title>${title}</title>\n<meta name='author' content='${author}'/>\n<meta name='date' content='${date}'/>\n<script type='text/javascript' src='https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML'></script>\n<link rel='stylesheet' href='http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/styles/default.min.css'>\n<script src='http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/highlight.min.js'></script>\n<script>hljs.initHighlightingOnLoad();</script>\n<link rel='stylesheet' type='text/css' href='${template-path}/${class}.css'>\n</head>\n<body>\n<header><h1>${title}</h1></header>\n${content}</body>\n</html>\n",
-				heading: "<h${level}>${content}</h${level}>\n",
-				paragraph: "<p>${content}</p>\n",
+				"document": "<!doctype html>\n<html>\n<head>\n<meta charset='UTF-8'>\n<title>${title}</title>\n<meta name='author' content='${author}'/>\n<meta name='date' content='${date}'/>\n<link rel='stylesheet' href='http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/styles/default.min.css'>\n<script src='http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/highlight.min.js'></script>\n<script>hljs.initHighlightingOnLoad();</script>\n<link rel='stylesheet' type='text/css' href='${template-path}/${class}.css'>\n</head>\n<body>\n<header><h1>${title}</h1></header>\n${content}</body>\n</html>\n",
+				"heading": "<h${level}>${content}</h${level}>\n",
+				"paragraph": "<p>${content}</p>\n",
 				"list item": "<li>${content}</li>\n",
 				"unordered list": "<ul>\n${content}</ul>\n",
 				"ordered list": "<ol>\n${content}</ol>\n",
-				emphasize: "<em>${content}</em>",
-				link: "<a href='${target}'>${content}</a>",
-				code: "<code>${content}</code>",
-				codeblock: "<figure>\n<pre><code>${code}</code></pre>\n<figcaption>${content}</figcaption>\n</figure>\n",
-				figure: "<figure>\n<img src='${source}' class='${classes}' />\n<figcaption>${content}</figcaption>\n</figure>\n",
-				video: "<figure>\n<video controls class='${classes}'><source src='${source}' type='${type}'>\n<figcaption>${content}</figcaption>\n</figure>\n",
-				math: "\\(${content}\\)",
-				mathblock: "<figure>\n\\[${math}\\]\n<figcaption>${content}</figcaption>\n</figure>\n",
+				"emphasize": "<em>${content}</em>",
+				"link": "<a href='${target}'>${content}</a>",
+				"code": "<code>${content}</code>",
+				"codeblock": "<figure>\n<pre><code class='${language}'>${code}</code></pre>\n<figcaption>${content}</figcaption>\n</figure>\n",
+				"figure": "<figure>\n<img src='${source}' class='${classes}' />\n<figcaption>${content}</figcaption>\n</figure>\n",
+				"video": "<figure>\n<video controls class='${classes}'><source src='${source}' type='${type}'>\n<figcaption>${content}</figcaption>\n</figure>\n",
+				"math": "${html}",
+				"mathblock": "<figure>\n${html}\n<figcaption>${content}</figcaption>\n</figure>\n",
+				"chapter": "<section class='chapter'>\n${content}</section>\n",
+				"section": "<section class='section'>\n${content}</section>\n",
+				"table": "<table class='${classes}'>\n${rows}<caption>${content}</caption>\n</table>\n",
+				"table row": "<tr>${content}</tr>\n",
+				"table header": "<th>${content}</th>",
+				"table cell": "<td>${content}</td>",
 			}, /* tslint:enable:no-invalid-template-strings max-line-length */
 		}
 		this.template = template as Template
